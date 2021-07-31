9eb02e8b9f8ba783582c57477c124bb1cd35b7df
huangxxiao <huangxxiao@yonyou.com>
2021-07-31 15:08:29 +0800

    删除了hello world

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 // https://bytedance.feishu.cn/base/app8Ok6k9qafpMkgyRbfgxeEnet?table=tblEnSV2PNAajtWE&view=vewJHSwJVd
 // https://juejin.cn/post/6844903986886098951
 export default {
